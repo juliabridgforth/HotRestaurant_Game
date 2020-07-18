955895d8ff906baeda5a53050c89b49e60187f34
Blackbird <ksilva.rod@gmail.com>
2020-07-18 10:40:32 -0500

    basic server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+
+
+var express = require("express");
+var path = require ("path")
+
+var app = express();
+
+
+var PORT =8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+//WAITING LIST /RESERVATION ARRAY//
+ 
