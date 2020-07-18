b9845ea7603d699344391170383463a07e50d87e
Blackbird <ksilva.rod@gmail.com>
2020-07-18 11:19:39 -0500

    adding table data.js

---
+++ tableData.js
@@ -0,0 +1,40 @@
+var tableArray = [
+    {
+      customerName: "Jarrett",
+      customerEmail: "jarrett@example.com",
+      customerID: "jarrett01",
+      phoneNumber: "000-000-0000"
+    },
+
+    {
+      customerName: "Karla",
+      customerEmail: "karla@example.com",
+      customerID: "karla02",
+      phoneNumber: "000-000-0000"
+    },
+
+    {
+      customerName: "Obi",
+      customerEmail: "obi@example.com",
+      customerID: "obi03",
+      phoneNumber: "000-000-0000"
+    },
+
+    {
+      customerName: "R2D2",
+      customerEmail: "r2d2d@example.com",
+      customerID: "r2d204",
+      phoneNumber: "000-000-0000",
+    },   
+     
+    {
+      customerName: "Anakin",
+      customerEmail: "anakin@example.com",
+      customerID: "anakin05",
+      phoneNumber: "000-000-0000"
+    }    
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tableArray;
+  
